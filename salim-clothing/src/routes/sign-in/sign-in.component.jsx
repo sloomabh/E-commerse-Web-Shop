fa2030fc5ed678fb149a2b387aa6e1aa0b52d103
salimbh <sloomabenhalima95@gmail.com>
2022-11-10 00:41:57 +0900

    setting up user documents

--- salim-clothing/src/routes/sign-in/sign-in.component.jsx
+++ salim-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,14 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import React from "react";
 
 const SignIn = () => {
   const logGoogleUser = async () => {
-    const res = await signInWithGooglePopup();
-    console.log(res);
+    // Destructering  {user}=response
+    const { user } = await signInWithGooglePopup();
+    createUserDocumentFromAuth(user);
   };
   return (
     <div>
