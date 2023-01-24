b903736b81552af3e1194533a38fc7914840c2ba
pej.benhalima <pej.benhalima@gmail.com>
2023-01-24 17:49:59 +0900

    typing create Action

---
+++ src/store/categories/categories.types.ts
@@ -0,0 +1,25 @@
+enum CATEGORIES_ACTION_TYPES {
+  // SET_CATEGORIES: "categories/SET_CATEGORIES_MAP",
+  FETCH_CATEGORIES_START = "category/FETCH_CATEGORIES_START",
+  FETCH_CATEGORIES_SUCCESS = "category/FETCH_CATEGORIES_SUCCESS",
+  FETCH_CATEGORIES_FAILED = "category/FETCH_CATEGORIES_FAILED",
+}
+
+export default CATEGORIES_ACTION_TYPES;
+
+export type CategoryItem = {
+  id: number;
+  imageUrl: string;
+  name: string;
+  price: number;
+};
+
+export type Category = {
+  title: string;
+  imageUrl: string;
+  items: CategoryItem[];
+};
+
+export type CategoryMap = {
+  [key: string]: CategoryItem[];
+};
